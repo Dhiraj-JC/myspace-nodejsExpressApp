e9fa8d645479ab41594766d6cda4b2b282359102
Dhiraj-JC <dhiraj@trainitsolutions.com>
2023-01-24 13:23:45 +0530

    first commit

---
+++ Models/product.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require('mongoose');
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required:true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = model('Product',productSchema);
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { connect } = require('mongoose');
+const Product = require('./Models/product');
+
+dotenv.config();
+connect(
+  process.env.DB_CONNECTION_STRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Database connection established');
+    }
+  }
+);
+
+const app = express();
+
+app.use(express.json());
+
+//Create product
+app.post('/products', async (req, res) => {
+  const product = req.body;
+
+  try {
+    const productEntity = new Product({
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        quantity: product.quantity
+    });
+
+    await productEntity.save();
+    res.json(productEntity);
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('listening on port 8000');
+});
